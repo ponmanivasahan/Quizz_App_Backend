824aec50dc649573fb950038c67adf107236044d
ponmanivasahan <vasupks01@gmail.com>
2026-08-15 09:53:14 +0530

    changes made in render

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,13 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME   
+    database: process.env.DB_NAME,
+    ssl: process.env.DB_SSL === "true"   
+        ? { rejectUnauthorized: true}
+        : undefined
 });
 db.connect((err)=>{
     if(err){
